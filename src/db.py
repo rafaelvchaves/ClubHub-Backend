dd8db11a42553463c879f1be3c8cc5e862081e7a
amandreasen <57854473+amandreasen@users.noreply.github.com>
2019-11-17 20:29:42 -0500

    added user-club relationship

--- src/db.py
+++ src/db.py
@@ -2,6 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+association_club_user = db.Table('association_club_user', db.Model.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('club_id', db.Integer, db.ForeignKey('club.id'))
+)
+
 class Club(db.Model):
     __tablename__ = 'club'
     id = db.Column(db.Integer, primary_key=True)
@@ -9,14 +14,14 @@ class Club(db.Model):
     #tags
     level = db.Column(db.String, nullable=False)
     application_required = db.Column(db.Boolean)
-    #interested_users
+    interested_users = db.relationship('User',secondary=association_club_user,back_populates='favorite_clubs')
     
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     netid = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
-    #favorite_clubs
+    favorite_clubs = db.relationship('Club',secondary=association_club_user,back_populates='interested_users')
     #created_posts
     #liked_posts
     
