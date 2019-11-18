d37d52b6f58afa8d439c9eca341a58498f0d67b2
rafaelchaves <rafaelvchaves@gmail.com>
2019-11-18 13:41:53 -0500

    Added serialize() methods

--- src/db.py
+++ src/db.py
@@ -20,7 +20,7 @@ class Club(db.Model):
     level = db.Column(db.String, nullable=False)
     application_required = db.Column(db.Boolean)
     interested_users = db.relationship('User', secondary=association_club_user, back_populates='favorite_clubs')
-    # tags
+    # tags = db.Column(db.String, nullable=False)
 
     def __init__(self, **kwargs):
         self.name = kwargs.get('name')
@@ -28,6 +28,16 @@ class Club(db.Model):
         self.level = kwargs.get('level')
         self.application_required = kwargs.get('application_required')
         self.interested_users = []
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'level': self.level,
+            'application_required': self.application_required,
+            # 'interested_users': [u.serialize() for u in self.interested_users]
+        }
 
     
 class User(db.Model):
@@ -44,6 +54,23 @@ class User(db.Model):
         self.password = kwargs.get('password')
         self.favorite_clubs = []
         self.created_posts = []
+        self.liked_posts = []
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'netid': self.netid,
+            'favorite_clubs': [c.serialize() for c in self.favorite_clubs],
+            'created_posts': [p.serialize() for p in self.created_posts],
+            'liked_posts': [p.serialize() for p in self.liked_posts]
+        }
+
+    def serialize_no_posts(self):
+        return {
+            'id': self.id,
+            'netid': self.netid,
+            'favorite_clubs': [c.serialize() for c in self.favorite_clubs]
+        }
     
 class Post(db.Model):
     __tablename__ = 'post'
@@ -51,11 +78,21 @@ class Post(db.Model):
     title = db.Column(db.String, nullable=False)
     body = db.Column(db.String, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    liked_posts = db.relationship('User', secondary=association_user_post, back_populates='liked_posts')
+    interested_users = db.relationship('User', secondary=association_user_post, back_populates='liked_posts')
     #type
 
     def __init__(self, **kwargs):
         self.title = kwargs.get('title')
         self.body = kwargs.get('body')
+        self.interested_users = []
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'body': self.body,
+            'author_id': self.author_id,
+            'interested_users': [u.serialize_no_posts() for u in self.interested_users]
+        }
 
 # class Tag(db.Model)
