bfb77931a3a35280b4c0dc6e513e53f6009b15ca
amandreasen <57854473+amandreasen@users.noreply.github.com>
2019-11-17 20:14:13 -0500

    Added non-relational attributes

--- src/db.py
+++ src/db.py
@@ -1,3 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Club(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String, nullable=False)
+    #tags
+    level = db.Column(db.String, nullable=False)
+    application_required = db.Column(db.Boolean)
+    #interested_users
+    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    netid = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    #favorite_clubs
+    #created_posts
+    #liked_posts
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    #author
+    #type
+    body = db.Column(db.String, nullable=False)
+    #interested_users
+    
+#class Tag(db.Model)
